3cfb880a2d9cb0e82a398ece7726d9f3803379d0
guoxp <13263153721@189.cn>
2023-11-13 19:56:17 +0800

    fix compile
    Change-Id: I8313906a9571c1b314d17e92e7f1bc0824a9bef3

--- src/pages/xh/monitor/Form/index.tsx
+++ src/pages/xh/monitor/Form/index.tsx
@@ -3,12 +3,12 @@ import React, { useContext, useEffect, useState } from 'react';
 
 import { Button, Card, Col, Form, Input, message, Modal, Row, Select, Space } from 'antd';
 import { useTranslation } from 'react-i18next';
-
+import queryString from 'query-string';
 import { CommonStateContext } from '@/App';
 import { addAsset, getAssetDefaultConfig, getAssetsIdents, getAssetsStypes, updateAsset, getAssetsByCondition } from '@/services/assets';
-import { createMonitor,} from '@/services/manage';
+import { createXhMonitor,getXhMonitor,updateXhMonitor} from '@/services/manage';
+import { useLocation } from 'react-router-dom';
 
-const {TextArea} = Input
 
 export default function (props: { initialValues: object; initParams: object; mode?: string }) {
   const { t } = useTranslation('assets');
@@ -20,6 +20,12 @@ export default function (props: { initialValues: object; initParams: object; mod
   const [identList, setIdentList] = useState([]);
   const [params, setParams] = useState<{ label: string; name: string; editable?: boolean; password?: boolean; items?: [] }[]>([]);
   const [form] = Form.useForm();
+  const { search } = useLocation();
+  const { id } = queryString.parse(search);
+  const { action } = queryString.parse(search);
+  const [monitor, setMonitor] = useState<any>(null);
+  
+
 
   const panelBaseProps: any = {
     size: 'small',
@@ -28,12 +34,16 @@ export default function (props: { initialValues: object; initParams: object; mod
 
 
   useEffect(() => {
+
+    if(action==null || "addeditview".indexOf(action+"")<0){
+      history.back()
+    }
     const param = {
       page: 1,
       limit: 10000,
     };
     getAssetsStypes().then((res) => {
-      const items = res.dat
+      const assetTypes = res.dat
         .map((v) => {
           return {
             value: v.name,
@@ -41,35 +51,62 @@ export default function (props: { initialValues: object; initParams: object; mod
             ...v,
           };
         })
-        .filter((v) => v.name !== '主机'); //探针自注册的不在前台添加
-      setAssetTypes(items);
-    });
+        // .filter((v) => v.name !== '主机'); //探针自注册的不在前台添加
+      setAssetTypes(assetTypes);
+      getAssetsByCondition(param).then((res) => {
+        let options = new Array;
+        res.dat.list.map((v) => {
+          options.push({
+            value: v.id,
+            label: v.name + "[" + v.type + "]"
+          });
+        })
+        setAssetOptions(options);
+        setAssetList(res.dat.list);
+        if(id!=null && id.length>0 && id!="null"){
+          getXhMonitor(id).then(({dat})=>{
+            if(dat!=null){
+              setMonitor(dat)
+              let config = dat["config"];
+              delete dat["config"];
+              if(config!=null && config.length>0){
+                let configJson = JSON.parse(config);
+                dat = {...configJson,...dat};
+              }              
+              form.setFieldsValue(dat);
+                setTimeout(()=>{
+                  genForm(res.dat.list,assetTypes);
+                },1000)           
+            }
+          })
+        }
+      });
 
-    getAssetsByCondition(param).then((res) => {
-      let options = new Array;
-      const items = res.dat.list.map((v) => {
-        options.push({
-          value: v.id,
-          label: v.name + "[" + v.type + "]"
-        });
-      })
-      setAssetOptions(options);
-      setAssetList(res.dat.list);
     });
+    getAssetsIdents().then((res) => {
+      const items = res.dat.map((v) => {
+        return {
+          value: v.id,
+          label: v.ident,
+        };
+      });
+      setIdentList(items);
+    });
+    
+    
+    
 
   }, []);
 
-  const genForm = () => {
+  const genForm = (assetList,assetTypes) => {
     console.log("genForm")
     let formValue = form.getFieldsValue();
     console.log(formValue);
     const asset:any = assetList.find((v:any) => v.id === formValue["asset_id"]);
     if (asset) {
-
       form.setFieldsValue({ type: asset.type });
       const assetType:any = assetTypes.find((v) => v.name === asset.type);
       setParams(assetType.form || []);
-      // genDefaultConfig()
     }
   };
   
@@ -78,6 +115,7 @@ export default function (props: { initialValues: object; initParams: object; mod
       return (
         <Select
           style={{ width: '100%' }}
+          disabled={action=="view"}
           options={v.items.map((v) => {
             return { label: v, value: v };
           })}
@@ -85,50 +123,39 @@ export default function (props: { initialValues: object; initParams: object; mod
       );
     }
     if (v.password) {
-      return <Input.Password placeholder={`填写${v.label}`} />;
+      return <Input.Password disabled={action=="view"} placeholder={`填写${v.label}`} />;
     }
-    return <Input placeholder={`填写${v.label}`} />;
+    return <Input disabled={action=="view"} placeholder={`填写${v.label}`} />;
   };
 
   const submitForm = async (values) => {
-    // values.group_id = curBusiId;
-    //debugger;
-    //values.params = JSON.stringify(values);
-
     console.log("submitForm",values);
-    createMonitor(values).then(res=>{
-      message.success('操作成功');
-      history.back();
-    })
-    // values.organization_id = organizationId;
-    // if (props.mode === 'edit') {
-    //   await updateAsset(values);
-    // } else {
-    //   await addAsset(values);
-    // }
-    
+    let config ={};
+    if(params!=null && params.length>0){
+      
+      for(let param in params){
+        let group = params[param];
+        config[group.name]= values[group.name];
+        delete values[group.name];
+      }
+      
+    }
+    if(id!=null && id.length>0 && id!="null"){
+      values.id = parseInt(""+id);
+      values["config"] = JSON.stringify(config);      
+      updateXhMonitor(values).then(res=>{
+        message.success('修改成功');
+        location.href = "/xh/monitor"
+      })
+    }else{
+      values["config"] = JSON.stringify(config);   
+      createXhMonitor(values).then(res=>{
+        message.success('操作成功');
+        location.href = "/xh/monitor?assetId="+values["asset_id"]
+      })
+    } 
   };
 
-  const genDefaultConfig = () => {
-    const name = form.getFieldValue('type');
-    // debugger;
-    const data = form.getFieldsValue();
-    // if (data.configs) {
-    //   Modal.confirm({
-    //     title: '将会覆盖原有配置,是否继续?',
-    //     onOk: () => {
-    //       delete data.configs;
-          getAssetDefaultConfig(name, data).then((res) => {
-            form.setFieldsValue({ configs: res.dat.content });
-          });
-        // },
-      // });
-    // } else {
-    //   getAssetDefaultConfig(name, data).then((res) => {
-    //     form.setFieldsValue({ configs: res.dat.content });
-    //   });
-    // }
-  };
 
   return (
     <Form
@@ -137,7 +164,7 @@ export default function (props: { initialValues: object; initParams: object; mod
       layout='vertical'
       onFinish={submitForm}
       onValuesChange={() => {
-           genForm();
+           genForm(assetList,assetTypes);
       }}
     >
       <Form.Item hidden name='id'>
@@ -150,18 +177,18 @@ export default function (props: { initialValues: object; initParams: object; mod
         <Card {...panelBaseProps} title={t('basic')}>
           <Row gutter={10}>
             <Col span={8}>
-              <Form.Item label='资产名称' name='asset_id' rules={[{ required: true }]}>
-                <Select style={{ width: '100%' }}  showSearch options={assetOptions} placeholder='请选择资产' disabled={props.mode === 'edit'} />
+              <Form.Item label='资产名称' name='asset_id' rules={[{ required: true }]} >
+                <Select style={{ width: '100%' }} disabled={action!="add"} showSearch options={assetOptions} placeholder='请选择资产'  />
               </Form.Item>
             </Col>
             <Col span={8}>
-              <Form.Item label='监控名称' name='name' rules={[{ required: true }]}>
-                <Input placeholder='请输入资产名称' />
+              <Form.Item label='监控名称' name='monitoring_name' rules={[{ required: true }]}>
+                <Input placeholder='请输入资产名称' disabled={action=="view"} />
               </Form.Item>
             </Col>
             <Col span={8}>
               <Form.Item label='数据源类型' name='datasource_id' rules={[{ required: true }]}>
-                <Select options={[{
+                <Select disabled={action=="view"} options={[{
                     value:1,label:'普罗米修斯'
                 }]}></Select>
               </Form.Item>
@@ -169,15 +196,15 @@ export default function (props: { initialValues: object; initParams: object; mod
           </Row>
           <Row gutter={10}>
             <Col span={12}>
-              <Form.Item label='监控脚本' name='configs' rules={[{ required: false }]}>
-                <TextArea placeholder='请输入监控脚本'  rows={5}/>
+              <Form.Item  label='监控脚本' name='monitoring_sql' rules={[{ required: false }]}>
+                <Input.TextArea placeholder='请输入监控脚本'  rows={5} disabled={action=="view"}/>
               </Form.Item>
             </Col>
             <Col span={12}>
               <Row gutter={10}>
                 <Col span={24}>
                 <Form.Item label='状态' name='status'>
-                  <Select style={{ width: '100%' }} options={
+                  <Select style={{ width: '100%' }} disabled={action=="view"} options={
                     [
                       { value: 0, label: '正常' }, { value: 1, label: '失效' }
                     ]
@@ -186,8 +213,8 @@ export default function (props: { initialValues: object; initParams: object; mod
                 </Form.Item>
                 </Col>
                 <Col span={24}>
-                  <Form.Item label='采集器' name='ident'>
-                    <Select style={{ width: '100%' }} options={identList} />
+                  <Form.Item label='采集器' name='target_id'>
+                    <Select style={{ width: '100%' }} disabled={action=="view"} options={identList} />
                   </Form.Item>
                 </Col>
               </Row>
@@ -196,7 +223,7 @@ export default function (props: { initialValues: object; initParams: object; mod
           <Row gutter={10}>
             <Col span={12}>
               <Form.Item label='描述' name='memo'>
-                <TextArea placeholder='填写描述' />
+                <Input.TextArea placeholder='填写描述' disabled={action=="view"} />
               </Form.Item>
             </Col>
           </Row>
@@ -214,27 +241,6 @@ export default function (props: { initialValues: object; initParams: object; mod
                 </Col>
               );
             })}
-            {/* <Col span={12}>
-              <Form.Item label='探针' name='ident'>
-                <Select style={{ width: '100%' }} options={identList} />
-              </Form.Item>
-            </Col> */}
-            {/* <Col span={24}>
-              <Form.Item
-                label={
-                  <Space>
-                    采集配置
-                    <Button type='primary' icon={<FileAddOutlined />} size='small' onClick={genDefaultConfig}>
-                      自动生成
-                    </Button>
-                  </Space>
-                }
-                name='configs'
-                rules={[{ required: true }]}
-              >
-                <CodeMirror height='200px' extensions={[StreamLanguage.define(toml)]}></CodeMirror>
-              </Form.Item>
-            </Col> */}
           </Row>
         </Card>
       </div>
@@ -242,9 +248,16 @@ export default function (props: { initialValues: object; initParams: object; mod
         <Form.Item >
           <div className='bottom_button'>
           <Space>
-            <Button type='primary' htmlType='submit'>
-              保存
-            </Button>
+            {action!=="view" && (
+              <Button type='primary' htmlType='submit'  disabled={action=="view"}>
+                保存
+              </Button>
+            )
+            
+            
+            
+            }
+            
             <Button
               onClick={() => {
                 window.location.href="/xh/monitor"
